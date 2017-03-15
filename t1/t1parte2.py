39f905277e464acbcd41c67adeba42c81ffc02d4
GHVieira <ghvieira@inf.ufsm.br>
2017-03-15 19:02:47 -0300

    Update em t1parte2

--- t1/t1parte2.py
+++ t1/t1parte2.py
@@ -10,4 +10,14 @@ def funcao_equacao(l1):
 
 #3 - Crie uma função que receba uma lista de nomes e retorne outra lista com somente aqueles nomes que terminarem com a letra 'a'.
 def procura_letra(l1):
-	return list()
+	return list(filter(lambda x: x[-1] == 'a', l1))
+
+
+#4 - Escreva uma função que, dada uma lista de idades de pessoas no ano atual, retorne uma lista somente com as idades de quem nasceu depois de 1970. Para testar a condição, sua função deverá subtrair a idade do ano atual. Exemplo de uso:
+ def apos_70(l1):
+ 	return list(filter(lambda x: x < 2017 - 1970, l1));
+
+
+ #5 - O código abaixo é escrito em Python imperativo. Escreva um código equivalente usando programação funcional.
+ 	def dobra_valor(l1):
+ 		return list(map(lambda x: x*2, l1))
