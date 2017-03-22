5c77da6bcf1f508a1cc1ca2403ad60e40818dd4a
GHVieira <ghvieira@inf.ufsm.br>
2017-03-21 21:28:03 -0300

    Inicio da parte 1 de

---
+++ t2/t2parte1.py
@@ -0,0 +1,24 @@
+
+
+
+#1 - Defina uma função addSuffix(suf,nomes) que retorne a lista de nomes com o sufixo suf adicionado.
+def addSufix(suf, nomes):
+    return [nome + suf for nome in nomes]
+
+#2 - Escreva uma função countShorts(words), que receba uma lista de palavras e retorne a quantidade de palavras dessa lista que possuem menos de 5 caracteres.
+
+#3 - Defina uma função stripVowels(s) que receba uma string e retire suas vogais.
+
+#4 - Defina uma função hideChars(s) que receba uma string, possivelmente contendo espaços, e que retorne outra string substituindo os demais caracteres por '-', mas mantendo os espaços.
+
+#5 - Defina uma função que receba um número n e retorne uma tabela de números de 1 a n e seus quadrados.
+
+#6 - Defina uma função que receba uma lista de palavras e retorne uma string contendo o primeiro e o último caracter de cada palavra da lista.
+
+#7 - Defina uma função myZip(l1,l2) que reproduza o comportamento da função zip do Python.
+
+#8 - Escreva uma função enumerate(words) que numere cada palavra da lista recebida.
+
+#9 - Escreva uma função isBin(s) que verifique se a string recebida representa um número binário.
+
+#10 - Escreva uma função bin2dec(digits), que receba uma lista de dígitos representando um número binário e retorne seu equivalente em decimal.
