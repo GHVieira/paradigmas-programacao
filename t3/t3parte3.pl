43cc867fb192bc1473b19f52ae629e00c487ce8a
GHVieira <ghvieira@inf.ufsm.br>
2017-04-05 20:14:18 -0300

    Atualização no code

--- t3/t3parte3.pl
+++ t3/t3parte3.pl
@@ -1,12 +1,8 @@
 #Questão 8 da OBI de 2016 - Iniciação Nível 1 - Fase 1
 
 
-#Para ter traves, precisa-se no mínimo 2 postes, então a regra caso tenha apenas 1 poste
-adiciona_traves(1, 0).
-#Recursiva que vai ser chamada X vezes sendo X o número de postes-1
-#Cada vez, que for chamada, o número de traves R aumenta em 3 e vai acumulando
-adiciona_traves(P, R) :- P>1, P1 is P-1, adiciona_traves(P1,R1), R is R1+3.
+#A partir de nº de postes > 1, a cada poste soma-se 3 travas
+#Se 3 travas, 2 conjuntos de 3 travas
+#nº total de travas = (nº total de postes -1) * 3
 
-
-#Função principal que recebe número de postes(P) e travas(T) e retorna TRUE caso os números batam
-calcula_traves(P, T) :- adiciona_traves(P, R), T=:=R. 
+calcula_traves(P, T) :- P1 is P - 1, T1 is 3*P1,  T=:=T1. 
